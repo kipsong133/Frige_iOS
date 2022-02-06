4efaaf5248dd847ed0f95126afb91187b4297433
KimWooSung <kipsong133@gmail.com>
2022-02-06 11:12:26 +0900

    Initial Commit

---
+++ Frige_iOS/Frige_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frige_iOSApp.swift
+//  Frige_iOS
+//
+//  Created by 김우성 on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct Frige_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
